4567894184cd536b3d1db0845f45c7806046f669
JT Park <bajutae@gmail.com>
2022-03-18 19:32:33 +0900

    mod readme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,69 +1,75 @@
-const routes = [
-  "HOME"
-];
-
-const createSidebar = () => {
-  const sidebar = {};
-  for (const route of routes) {
-    Object.assign(sidebar, require("../" + route));
-  }
-  return sidebar;
-};
-
 module.exports = {
-  head: [
-    [
-      "script",
-      {
-        "data-ad-client": "ca-pub-6192551181326997",
-        async: true,
-        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      }
-    ]
-  ],
-  title: "DO IT Right now",
-  description: "",
-  "google-site-verification": "J993cuJJ-yFppfJKY8rMx3SzsN-3ZMBEM4uY7Jj075A",
+  title: "Do IT right now",
+  description: "desc",
   themeConfig: {
     nav: [
-      { text: "Tag", link: "/Tag/" },
-      // { text: "개발일기", link: "/dev-report/" },
-      {
-        text: "Info",
-        items: [
-          // { text: "About", link: "/about/" },
-          { text: "Github", link: "https://github.com/Kyounghwan01" },
-          {
-            text: "블로그 건의하기",
-            link: "https://github.com/Kyounghwan01/blog/issues/20"
-          }
-        ]
-      }
+      { text: 'k8s', link: '/k8s/' },
+      { text: 'External', link: 'https://google.com' }
     ],
-    sidebar: createSidebar(),
-    lastUpdated: "최근변경일"
+    sidebar: getSidebarArr()
   },
-  base: "/blog/",
-  plugins: [
-    // [
-    //   "vuepress-plugin-google-adsense",
-    //   {
-    //     google_ad_client: "ca-pub-6192551181326997", // ca-pub-0000000000000000
-    //     enable_page_level_ads: true
-    //   }
-    // ],
-    // [
-    //   "@vuepress/google-analytics",
-    //   {
-    //     ga: "UA-172169202-1" // UA-00000000-0
-    //   }
-    // ],
-    ["@vuepress/back-to-top"],
-    ["vuepress-plugin-code-copy"],
-    ["sitemap", { hostname: "https://github.com/bajutae/blog/" }],
-    ["@vuepress/last-updated"]
-  ]
-  // markdown: {
-  //   lineNumbers: true
-  // }
+  //가장 중요한 부분!
+  //<username>.github.io 뒤에 주소가 붙으시면
+  //아래와 같이 뒤 붙는 주소를 넣어주셔야합니다.
+  //안그러면 css 가 반영이 안되요!! 꼭꼭 넣어주세요
+  base: "/blog/"
 };
+
+function getSidebarArr() {
+  var fs = require("fs");
+  var docsPath = __dirname + "/../";
+  var sidebarArr = [];
+  var HomeFilelist = [];
+  var filelist = fs.readdirSync(docsPath);
+  filelist.forEach(function(file) {
+    if (file === ".vuepress") return;
+    var stat = fs.lstatSync(docsPath + "/" + file);
+    if (stat.isDirectory()) {
+      // directory
+      // title is file, children is readdirSync
+      var docsFolderPath = docsPath + "/" + file;
+      var list = fs.readdirSync(docsFolderPath);
+      sidebarArr.push(makeSidebarObject(file, list));
+    } else {
+      // NOT directory
+      // title is '/' children is file
+      HomeFilelist.push(file);
+    }
+  });
+  sidebarArr.unshift(makeSidebarObject("", HomeFilelist));
+  return sidebarArr;
+}
+function makeSidebarObject(folder, mdfileList) {
+  var path = folder ? "/" + folder + "/" : "/";
+  mdfileList = aheadOfReadme(mdfileList);
+  var tmpMdfileList = [];
+  // remove .md, add Path
+  mdfileList.forEach(function(mdfile) {
+    if (mdfile.substr(-3) === ".md") {
+      mdfile = mdfile.slice(0, -3) === "README" ? "" : mdfile.slice(0, -3);
+      tmpMdfileList.push(path + mdfile);
+    }
+  });
+  mdfileList = tmpMdfileList;
+  // remove folder prefix number
+  if (folder) {
+    var dotIdx = folder.indexOf(".");
+    var title = Number(folder.substr(0, dotIdx))
+      ? folder.substr(dotIdx + 1)
+      : folder;
+  } else {
+    title = "HOME";
+  }
+  return {
+    title: title,
+    children: mdfileList
+  };
+}
+function aheadOfReadme(arr) {
+  // ['1.test.md','README.md'] => ['README.md','1.test.md']
+  var readmeIdx = arr.indexOf("README.md");
+  if (readmeIdx > 0) {
+    arr.unshift(arr.splice(readmeIdx, 1)[0]);
+  }
+  return arr;
+}
--- docs/README.md
+++ docs/README.md
@@ -1,9 +1,12 @@
 ---
 home: true
 actionText: Get Started
-actionLink: /tag/
+actionLink: /k8s/
 features:
   - title: K8S
-    details: React v16 기본 및 이슈 해결 모음
+    details: dddd
+  - title: External
+    details: google    
+
 footer: Copyright 2022. bajutae all rights reserved.
 ---
